1a637a4cc7d3446a000d351321fa9d6c4f79d297
Nicholas J. Michalek <djphazer@gmail.com>
2025-02-20 17:53:18 -0500

    Version bump 0.99.8

--- src/apps/sequencer/Config.h
+++ src/apps/sequencer/Config.h
@@ -7,7 +7,7 @@
 #define CONFIG_VERSION_NAME             "PEW|former Alpha"
 #define CONFIG_VERSION_MAJOR            0
 #define CONFIG_VERSION_MINOR            99
-#define CONFIG_VERSION_REVISION         7
+#define CONFIG_VERSION_REVISION         8
 
 // Task priorities
 #define CONFIG_DRIVER_TASK_PRIORITY     5
